e588e8e22eeb0bb3b6e8054cc40fa83f9e2ee989
cb8ms <caio.benatti@8ms.com>
2025-04-15 12:56:17 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Default build directory for Vite
-    rollupOptions: {
-      input: "index.html",
-    },
+    outDir: "dist",
   },
 });
