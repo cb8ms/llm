26c72e7515df6908277d998aa62cd5233873db66
cb8ms <caio.benatti@8ms.com>
2025-04-15 16:15:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
     rollupOptions: {
-      input: {
-        main: "/index.html",
-      },
+      input: "index.html",  // Ensure the path is correct to the index.html file
     },
   },
 });
